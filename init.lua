e752af90480abc567c7413630fe15197387d9b0e
Rayan HADDD <rayan.haddad169@outlook.fr>
2024-03-29 19:44:45 +0100

    config update

--- init.lua
+++ init.lua
@@ -1,6 +1,35 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("catppuccin").setup({ transparent_background = true })
+local opt_test = {
+  integrations = {
+    aerial = true,
+    alpha = true,
+    cmp = true,
+    dashboard = true,
+    flash = true,
+    gitsigns = true,
+    headlines = true,
+    illuminate = true,
+    indent_blankline = { enabled = true },
+    leap = true,
+    lsp_trouble = true,
+    mason = true,
+    markdown = true,
+    mini = true,
+    native_lsp = {
+      enabled = true,
+      underlines = {
+        errors = { "undercurl" },
+        hints = { "undercurl" },
+        warnings = { "undercurl" },
+        information = { "undercurl" },
+      },
+    },
+  },
+  transparent_background = true,
+}
+
+require("catppuccin").setup(opt_test)
 vim.cmd.colorscheme("catppuccin")
 local user_config = { background_colour = "#000000" }
 require("notify").setup(user_config)
