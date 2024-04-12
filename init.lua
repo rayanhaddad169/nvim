275bf8deb8086ace82c7186862102a4096cf15be
Rayan HADDD <rayan.haddad169@outlook.fr>
2024-04-13 00:21:50 +0200

    cleaning config

--- init.lua
+++ init.lua
@@ -1,38 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-local opt = {
-  integrations = {
-    aerial = true,
-    alpha = true,
-    cmp = true,
-    dashboard = true,
-    flash = true,
-    gitsigns = true,
-    headlines = true,
-    illuminate = true,
-    indent_blankline = { enabled = true },
-    leap = true,
-    lsp_trouble = true,
-    mason = true,
-    markdown = true,
-    mini = true,
-    native_lsp = {
-      enabled = true,
-      underlines = {
-        errors = { "undercurl" },
-        hints = { "undercurl" },
-        warnings = { "undercurl" },
-        information = { "undercurl" },
-      },
-    },
-  },
-  transparent_background = true,
-}
 
-require("catppuccin").setup(opt)
+require("notify").setup({ background_colour = "#000000" })
+
 vim.cmd.colorscheme("catppuccin")
-local user_config = { background_colour = "#000000" }
-require("notify").setup(user_config)
+
 require("lspconfig").pylsp.setup({
   settings = {
     pylsp = {
@@ -47,38 +19,12 @@ require("lspconfig").pylsp.setup({
   },
 })
 
-local undotree = require("undotree")
-
-undotree.setup({
-  float_diff = true, -- using float window previews diff, set this `true` will disable layout option
-  layout = "left_bottom", -- "left_bottom", "left_left_bottom"
-  position = "left", -- "right", "bottom"
-  ignore_filetype = { "undotree", "undotreeDiff", "qf", "TelescopePrompt", "spectre_panel", "tsplayground" },
-  window = {
-    winblend = 30,
-  },
-  keymaps = {
-    ["j"] = "move_next",
-    ["k"] = "move_prev",
-    ["gj"] = "move2parent",
-    ["J"] = "move_change_next",
-    ["K"] = "move_change_prev",
-    ["<cr>"] = "action_enter",
-    ["p"] = "enter_diffbuf",
-    ["q"] = "quit",
-  },
-})
 vim.keymap.set("n", "<leader>u", require("undotree").toggle, { noremap = true, silent = true })
-local opts = {
-  filesystem = {
-    filtered_items = {
-      visible = false,
-      always_show = { -- remains visible even if other settings would normally hide it
-        ".zshrc",
-      },
-      show_hidden_count = true,
-      hide_dotfiles = true,
+
+require("conform").setup({
+  formatters = {
+    black = {
+      prepend_args = { "--line-length", "120" },
     },
   },
-}
-require("neo-tree").setup(opts)
+})
---
+++ lua/plugins/mason_plug.lua
@@ -0,0 +1,51 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    local mason = require("mason")
+
+    local mason_tool_installer = require("mason-tool-installer")
+
+    local masonlsp = require("mason-lspconfig")
+
+    local lint = require("lint")
+
+    local formatter = require("conform")
+
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint", -- python linter
+        "docformatter",
+      },
+    })
+
+    masonlsp.setup({
+      ensure_installed = { "pylsp", "dockerls" },
+    })
+
+    formatter.setup({
+      formatters_by_ft = {
+        python = { "isort", "black" },
+      },
+    })
+
+    lint.linters_by_ft = {
+      python = { "pylint" },
+    }
+  end,
+}
